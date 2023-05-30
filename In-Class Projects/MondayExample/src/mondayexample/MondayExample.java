f04d299a59f4080363681ee3449be82eeb87545d
pate5080 <pate5080@sheridancollege.ca>
2023-05-29 20:28:32 -0400

    MondayExample

--- In-Class Projects/MondayExample/src/mondayexample/MondayExample.java
+++ In-Class Projects/MondayExample/src/mondayexample/MondayExample.java
@@ -16,7 +16,6 @@ public class MondayExample {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("GitHub Operations");
-        System.out.println("Git Operations: Pull");
     }
     
 }
